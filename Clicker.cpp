bf97bfae5ea77e005c07fa6aab74b6ea480866f9
Jacky Romano <jacky.romano@gmail.com>
2023-03-19 16:46:39 +0200

    Fix tabs

--- Clicker.cpp
+++ Clicker.cpp
@@ -10,126 +10,126 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 
 void Clicker::Init(HWND hwnd)
 {
-	m_hwnd = hwnd;
+    m_hwnd = hwnd;
 
-	InitContextMenu();
-	InitClickSound();
-	InitMouseHook();
+    InitContextMenu();
+    InitClickSound();
+    InitMouseHook();
 
-	m_initialized = true;
+    m_initialized = true;
 }
 
 void Clicker::Show()
 {
-	if (!m_initialized) return;
+    if (!m_initialized) return;
 
-	POINT curPoint;
-	GetCursorPos(&curPoint);
-	int clicked = TrackPopupMenu(m_hMenu, TPM_RETURNCMD, curPoint.x, curPoint.y, 0, m_hwnd, NULL);
-	if (clicked != 0) {
-		switch (clicked) {
-		case Clicker::EXIT:
-			PostQuitMessage(0);
-			break;
-		case Clicker::ENABLE:
-			EnableClickFeedback();
-			break;
-		case Clicker::DISABLE:
-			DisableClickFeedback();
-			break;
-		case Clicker::ABOUT:
-			DialogBox(NULL, MAKEINTRESOURCE(IDD_ABOUTBOX), NULL, About);
-			break;
-		}
-	}
+    POINT curPoint;
+    GetCursorPos(&curPoint);
+    int clicked = TrackPopupMenu(m_hMenu, TPM_RETURNCMD, curPoint.x, curPoint.y, 0, m_hwnd, NULL);
+    if (clicked != 0) {
+        switch (clicked) {
+        case Clicker::EXIT:
+            PostQuitMessage(0);
+            break;
+        case Clicker::ENABLE:
+            EnableClickFeedback();
+            break;
+        case Clicker::DISABLE:
+            DisableClickFeedback();
+            break;
+        case Clicker::ABOUT:
+            DialogBox(NULL, MAKEINTRESOURCE(IDD_ABOUTBOX), NULL, About);
+            break;
+        }
+    }
 }
 
 void Clicker::EnableClickFeedback()
 {
-	m_enabled = true;
+    m_enabled = true;
 }
 
 void Clicker::DisableClickFeedback()
 {
-	m_enabled = false;
+    m_enabled = false;
 }
 
 LRESULT CALLBACK Clicker::LowLevelMouseProc(_In_ int    nCode, _In_ WPARAM wParam, _In_ LPARAM lParam)
 {
-	if (Clicker::Instance()->m_enabled) {
-		HMODULE hMod;
-		GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCTSTR)Clicker::LowLevelMouseProc, &hMod);
+    if (Clicker::Instance()->m_enabled) {
+        HMODULE hMod;
+        GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCTSTR)Clicker::LowLevelMouseProc, &hMod);
 
-		switch (wParam) {
-		case WM_LBUTTONDOWN:
-		case WM_RBUTTONDOWN:
-		case WM_MBUTTONDOWN:
-			PlaySound(Clicker::Instance()->m_clickWave, NULL, SND_MEMORY | SND_ASYNC | SND_SYSTEM);
-			break;
-		}
-	}
-	return CallNextHookEx(NULL, nCode, wParam, lParam);
+        switch (wParam) {
+        case WM_LBUTTONDOWN:
+        case WM_RBUTTONDOWN:
+        case WM_MBUTTONDOWN:
+            PlaySound(Clicker::Instance()->m_clickWave, NULL, SND_MEMORY | SND_ASYNC | SND_SYSTEM);
+            break;
+        }
+    }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
 void Clicker::InitContextMenu()
 {
-	m_hMenu = CreatePopupMenu();
-	AppendMenu(m_hMenu, MF_STRING, Clicker::EXIT, L"Exit");
-	AppendMenu(m_hMenu, MF_STRING, Clicker::ENABLE, L"Enable");
-	AppendMenu(m_hMenu, MF_STRING, Clicker::DISABLE, L"Disable");
-	AppendMenu(m_hMenu, MF_SEPARATOR, -1, L"");
-	AppendMenu(m_hMenu, MF_STRING, Clicker::ABOUT, L"About");
+    m_hMenu = CreatePopupMenu();
+    AppendMenu(m_hMenu, MF_STRING, Clicker::EXIT, L"Exit");
+    AppendMenu(m_hMenu, MF_STRING, Clicker::ENABLE, L"Enable");
+    AppendMenu(m_hMenu, MF_STRING, Clicker::DISABLE, L"Disable");
+    AppendMenu(m_hMenu, MF_SEPARATOR, -1, L"");
+    AppendMenu(m_hMenu, MF_STRING, Clicker::ABOUT, L"About");
 }
 
 void Clicker::InitClickSound()
 {
-	HANDLE hResInfo = FindResource(GetModuleHandle(NULL), L"IDI_CLICK", L"WAVE");
-	if (hResInfo == NULL) {
-		MessageBox(NULL, L"Failed to find click resource", L"ERROR", MB_ICONERROR | MB_OK);
-		PostQuitMessage(0);
-		return;
-	}
+    HANDLE hResInfo = FindResource(GetModuleHandle(NULL), L"IDI_CLICK", L"WAVE");
+    if (hResInfo == NULL) {
+        MessageBox(NULL, L"Failed to find click resource", L"ERROR", MB_ICONERROR | MB_OK);
+        PostQuitMessage(0);
+        return;
+    }
 
-	HANDLE hRes = LoadResource(NULL, (HRSRC)hResInfo);
-	if (hRes == NULL) {
-		MessageBox(NULL, L"Failed to load click resource", L"ERROR", MB_ICONERROR | MB_OK);
-		PostQuitMessage(0);
-		return;
-	}
+    HANDLE hRes = LoadResource(NULL, (HRSRC)hResInfo);
+    if (hRes == NULL) {
+        MessageBox(NULL, L"Failed to load click resource", L"ERROR", MB_ICONERROR | MB_OK);
+        PostQuitMessage(0);
+        return;
+    }
 
-	m_clickWave = (LPWSTR)LockResource(hRes);
+    m_clickWave = (LPWSTR)LockResource(hRes);
 
-	if (m_clickWave == NULL) {
-		MessageBox(NULL, L"Failed to lock resource", L"ERROR", MB_ICONERROR | MB_OK);
-		PostQuitMessage(0);
-	}
+    if (m_clickWave == NULL) {
+        MessageBox(NULL, L"Failed to lock resource", L"ERROR", MB_ICONERROR | MB_OK);
+        PostQuitMessage(0);
+    }
 }
 
 void Clicker::InitMouseHook()
 {
-	m_hhook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, NULL, 0);
-	if (m_hhook == NULL) {
-		MessageBox(NULL, L"Failed to install Mouse hook", L"ERROR", MB_ICONERROR | MB_OK);
-		PostQuitMessage(0);
-		return;
-	}
+    m_hhook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, NULL, 0);
+    if (m_hhook == NULL) {
+        MessageBox(NULL, L"Failed to install Mouse hook", L"ERROR", MB_ICONERROR | MB_OK);
+        PostQuitMessage(0);
+        return;
+    }
 }
 
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
 
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
 }
--- Clicker.h
+++ Clicker.h
@@ -6,39 +6,39 @@
 class Clicker
 {
 public:
-	enum MenuId {
-		EXIT = 0x1234,
-		ENABLE,
-		DISABLE,
-		ABOUT
-	};
+    enum MenuId {
+        EXIT = 0x1234,
+        ENABLE,
+        DISABLE,
+        ABOUT
+    };
 
-	static Clicker* Instance() {
-		if (m_instance == nullptr) {
-			m_instance = new Clicker;
-		}
-		return m_instance;
-	}
-	void Init(HWND hwnd);
-	void Show();
+    static Clicker* Instance() {
+        if (m_instance == nullptr) {
+            m_instance = new Clicker;
+        }
+        return m_instance;
+    }
+    void Init(HWND hwnd);
+    void Show();
 private:
-	bool m_initialized = false;
-	bool m_enabled = false;
-	HWND m_hwnd = NULL;
-	HMENU m_hMenu = NULL;
-	HHOOK m_hhook = NULL;
-	static Clicker* m_instance;
-	LPWSTR m_clickWave;
+    bool m_initialized = false;
+    bool m_enabled = false;
+    HWND m_hwnd = NULL;
+    HMENU m_hMenu = NULL;
+    HHOOK m_hhook = NULL;
+    static Clicker* m_instance;
+    LPWSTR m_clickWave;
 
-	Clicker() {}
-	~Clicker() {}
+    Clicker() {}
+    ~Clicker() {}
 
-	void EnableClickFeedback();
-	void DisableClickFeedback();
-	static LRESULT CALLBACK LowLevelMouseProc(_In_ int nCode, _In_ WPARAM wParam, _In_ LPARAM lParam);
-	void InitContextMenu();
-	void InitClickSound();
-	void InitMouseHook();
+    void EnableClickFeedback();
+    void DisableClickFeedback();
+    static LRESULT CALLBACK LowLevelMouseProc(_In_ int nCode, _In_ WPARAM wParam, _In_ LPARAM lParam);
+    void InitContextMenu();
+    void InitClickSound();
+    void InitMouseHook();
 };
 
 
